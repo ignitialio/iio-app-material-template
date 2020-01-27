1105663637508fbf982ac59531a503ad2f9e654a
Victor Andritoiu <devandrito@gmail.com>
2020-01-27 12:32:31 +0100

    bump services versions

--- swarm/deploy.sh
+++ swarm/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export IIOS_DLAKE_VERSION=3.4.1
-export IIOS_AUTH_VERSION=1.2.1
+export IIOS_DLAKE_VERSION=3.5.1
+export IIOS_AUTH_VERSION=1.3.0
 
 docker network create --opt encrypted -d overlay infra
 docker stack deploy -c swarm/docker-compose.yml iioat
